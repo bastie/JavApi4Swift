447c04144be339642885409c87a0fc5dba5aaf5f
Sͬeͥbͭaͭsͤtͬian <bastie@users.noreply.github.com>
2024-09-06 12:28:48 +0200

    add equals functions to java.util.Arrays

--- Sources/JavApi/util/Arrays.swift
+++ Sources/JavApi/util/Arrays.swift
@@ -6,20 +6,6 @@ extension java.util {
   
   /// Utility type to work with Arrays
   public class Arrays {
-
-    /// Fill the given array with element on all indices
-    /// - Parameters:
-    ///     - array to fill
-    ///     - element
-    func fill<T>(_ array: inout [T], _ element: T) {
-      for i in array.indices {
-        array[i] = element
-      }
-    }
-    public static func copyOf (_ original : [Int], _ newCount : Int) -> [Int] {
-      return Array (original: original, count: newCount)
-    }
-    
     public static func binarySearch (_ a : Array<Any>, _ e : ((Any) -> Bool)) -> Int {
       do {
         let index : Int = try a.binarySearch(predicate: e)
@@ -30,6 +16,103 @@ extension java.util {
       }
     }
     
+    public static func copyOf (_ original : [Int], _ newCount : Int) -> [Int] {
+      return Array (original: original, count: newCount)
+    }
+    
+    /// If all element of two array are in same order in both contained, these arrays are equals. If both arrays are ``nil`` these arrays are also same.
+    /// - Parameter actual array
+    /// - Parameter identical array
+    /// - Returns ``true`` if they are equals
+    public static func equals (_ actual : [UInt8]?, _ identical : [UInt8]?) -> Bool {
+      if nil == actual && nil == identical {
+        return true
+      }
+      if let actual, let identical {
+        return equals(actual, identical)
+      }
+      return false
+    }
+    
+    /// If all element of two array are in same order in both contained, these arrays are equals.
+    /// - Parameter actual array
+    /// - Parameter identical array
+    /// - Returns ``true`` if they are equals
+    public static func equals (_ actual : [UInt8], _ identical : [UInt8]) -> Bool {
+      guard actual.count == identical.count else {
+        return false
+      }
+      for (offset,_) in actual.enumerated() {
+        if actual[offset] == identical[offset] {
+          return false
+        }
+      }
+      return true
+    }
+    
+    /// If all element of two array are in same order in both contained, these arrays are equals. If both arrays are ``nil`` these arrays are also same.
+    /// - Parameter actual array
+    /// - Parameter identical array
+    /// - Returns ``true`` if they are equals
+    public static func equals (_ actual : [UInt8]?, _ identical : [Int]?) -> Bool {
+      if nil == actual && nil == identical {
+        return true
+      }
+      if let actual, let identical {
+        return equals(actual, identical)
+      }
+      return false
+    }
+    
+    /// If all element of two array are in same order in both contained, these arrays are equals.
+    /// - Parameter actual array
+    /// - Parameter identical array
+    /// - Returns ``true`` if they are equals
+    public static func equals (_ actual : [UInt8], _ identical : [Int]) -> Bool {
+      return equals(identical, actual)
+    }
+    
+    /// If all element of two array are in same order in both contained, these arrays are equals. If both arrays are ``nil`` these arrays are also same.
+    /// - Parameter actual array
+    /// - Parameter identical array
+    /// - Returns ``true`` if they are equals
+    public static func equals (_ actual : [Int]?, _ identical : [UInt8]?) -> Bool {
+      if nil == actual && nil == identical {
+        return true
+      }
+      if let actual, let identical {
+        return equals(actual, identical)
+      }
+      return false
+    }
+    
+    /// If all element of two array are in same order in both contained, these arrays are equals.
+    /// - Parameter actual array
+    /// - Parameter identical array
+    /// - Returns ``true`` if they are equals
+    public static func equals (_ actual : [Int], _ identical : [UInt8]) -> Bool {
+      guard actual.count == identical.count else {
+        return false
+      }
+      for (offset,_) in actual.enumerated() {
+        if actual[offset] == identical[offset] {
+          return false
+        }
+      }
+      return true
+    }
+
+
+
+    /// Fill the given array with element on all indices
+    /// - Parameters:
+    ///     - array to fill
+    ///     - element
+    func fill<T>(_ array: inout [T], _ element: T) {
+      for i in array.indices {
+        array[i] = element
+      }
+    }
     
     public static func fill (_ a : inout [Int], _ fromIndex : Int, _ toIndex : Int, _ e : Int) {
       for i in fromIndex..<toIndex {
