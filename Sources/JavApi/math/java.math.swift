8644dd0aa779da7c8ecf72ca26412b171f1258d3
Sͬeͥbͭaͭsͤtͬian <bastie@users.noreply.github.com>
2024-09-09 21:19:45 +0200

    set DOCC at right place

--- Sources/JavApi/math/java.math.swift
+++ Sources/JavApi/math/java.math.swift
@@ -2,7 +2,8 @@
  * SPDX-FileCopyrightText: 2023 - Sebastian Ritter <bastie@users.noreply.github.com>
  * SPDX-License-Identifier: MIT
  */
-/// The java.math namespace
+
 extension java {
+  /// The java.math namespace
   public enum math {}
 }
