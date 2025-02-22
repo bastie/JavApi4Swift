f7c81a62b361f564e1aa7385b710d1846b197e9f
Sͬeͥbͭaͭsͤtͬian <bastie@users.noreply.github.com>
2025-02-22 17:54:35 +0100

    not only

--- Sources/JavApi/no/no.swift
+++ Sources/JavApi/no/no.swift
@@ -2,6 +2,7 @@
  * SPDX-FileCopyrightText: 2025 - Sebastian Ritter <bastie@users.noreply.github.com>
  * SPDX-License-Identifier: MIT
  */
-/// The not only Java namespace
+/// The not only Java namespace is placed in result of I used mostly JavApi and find no other place
+/// A very very bad idea!
 ///
 public enum no {}
