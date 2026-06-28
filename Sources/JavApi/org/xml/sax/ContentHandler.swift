d02720d3657e314c32484490d512906d9ea81e38
Sebastian Ritter <bastie@users.noreply.github.com>
2026-06-28 14:14:53 +0200

    fix missing parameters in charachters function

--- Sources/JavApi/org/xml/sax/ContentHandler.swift
+++ Sources/JavApi/org/xml/sax/ContentHandler.swift
@@ -6,13 +6,13 @@
 extension org.xml.sax {
   
   /// - Since: SAX 1.0
-  @available(*, deprecated, renamed: "ContentHandler", message: "Deprecated since Java 5. Use ContentHandler instead")
+  @available(*, deprecated, renamed: "ContentHandler", message: "Use ContentHandler instead")
   public protocol DocumentHandler {
     func startDocument() throws (SAXException)
     func endDocument() throws (SAXException)
     func startElement(_ name: String, _ attributes: [String: String]) throws (SAXException)
     func endElement(_ name: String) throws (SAXException)
-    func characters(_ ch: String, _ start : Int, _ length: Int) throws (SAXException)
+    func characters(_ ch: String) throws (SAXException)
     func ignorableWhitespace(_ ch: [Character], _ start: Int, _ length: Int) throws (SAXException)
     func processingInstruction(_ target: String, _data: String) throws (SAXException)
     func setDocumentLocator(_ locator: Locator) throws (SAXException)
---
+++ Sources/JavApi/org/xml/sax/helper/org.xml.sax.helper.swift
@@ -0,0 +1,9 @@
+/*
+ * SPDX-FileCopyrightText: 2026 - Sebastian Ritter <bastie@users.noreply.github.com>
+ * SPDX-License-Identifier: 0BSD
+ */
+
+extension org.xml.sax {
+  /// The org.xml.sax.helper namespace
+  public enum elper {}
+}
