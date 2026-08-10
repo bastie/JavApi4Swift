a5fe00444d410a8564dbdb6d315b5848d7882b65
Sebastian Ritter <bastie@users.noreply.github.com>
2026-08-10 19:29:25 +0200

    documentation of EmptyStackException

--- Sources/JavApi/util/EmptyStackException.swift
+++ Sources/JavApi/util/EmptyStackException.swift
@@ -4,8 +4,13 @@
  */
 
 extension java.util {
+  
+  /// Exception throw to indicate `Stack` is empty.
+  ///
+  /// - See: ``Stack``
   open class EmptyStackException : RuntimeException, @unchecked Sendable {
     
+    /// Construct a instance without message.
     public override init () {
       super.init()
     }
